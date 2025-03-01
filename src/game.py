11f8a85871158c3725d30f97cb645365a3f1cecf
Gerson <gerson_a@hotmail.com>
2025-03-01 14:35:20 +0100

    version3 upd 1

--- src/game.py
+++ src/game.py
@@ -1,12 +1,8 @@
 import time
-from code import InteractiveConsole
-from linecache import clearcache
-from operator import length_hint
-from traceback import print_tb
+
 
 from .grid import Grid
 from .move_commands import show_score, inventory, showlist, print_status
-from .pickups import Item
 from .player import Player
 from . import pickups
 
@@ -20,6 +16,7 @@ shovel = 0
 key = 0
 treasure = 0
 steps = 0
+jumpvalue=1
 total_fruits=8    # Total fruits in the grid
 total_fruits_in_basket=0  # total fruits collected in the basket
 exit_found = 'noexit'
@@ -54,43 +51,50 @@ pickups.randomtreasure(g)
 pickups.exit_game(g)
 
 
-g.set(50, 3, ' [ Off ]')  # Start with no keys
-g.set(50, 6, 0)  # clear Key
-g.set(50, 7, 0)  # clear points
+g.set(50, 2, ' [ Off ]')  # Start with no keys
+g.set(50, 5, 0)  # clear Key
+g.set(50, 6, 0)  # clear treasure
+g.set(50, 8, 0)  # clear points
 
 
 # TODO: flytta denna till en annan fil ???
 
-def move_point(score, xi, yi, shovel, key, treasure, total_fruits_in_basket,exit_found):
+def move_point(score, xi, yi, shovel, key, treasure, total_fruits_in_basket,exit_found,jumpvalue):
 
     maybe_item = g.get(player.pos_x + xi, player.pos_y+yi)
 
     if shovel==1:
-        g.set(50, 3, ' [ On ]' )
+        g.set(50, 2, ' [ On ]' )
+
+    if maybe_item == wall:
+        exit_found = wall
+        jumpvalue=1
+        player.move(0, 0)
+
+    # Cannot move - extern wall
+        g.set(37, 10, f" * Can't move - there's an extern wall {wall} **")
+        return score, shovel, key, treasure, total_fruits_in_basket, exit_found,jumpvalue
+
 
     if maybe_item == internwall:
         if shovel==0:
-            g.set(37, 9, f" * Can't move - there's an intern wall {internwall} ")
-            player.move(0,0)
+            player.move(0, 0)
+            g.set(37, 10, f" * Can't move - there's an intern wall {internwall} ")
+
         else:
-            print(">> >> >>  Breaking the wall")
             player.move(xi, yi)
+            print(">> >> >>  Breaking the wall")
             g.set(player.pos_x, player.pos_y, g.empty)
             shovel=0
-            g.set(50, 3, ' [ Off ]')
-
-    elif maybe_item == wall:
-    # Can no move - extern wall
-        g.set(37, 9, f" * Can't move - there's an extern wall {wall} **")
-        player.move(0, 0)
+            g.set(50, 2, ' [ Off ]')
 
 
     elif maybe_item == trap_a:
+        player.move(xi, yi)
         # I) Fällor - introducera valfri fälla till spelplanen. Om man går på en ruta med en fälla ska man förlora 10 poäng.
         #    Fällan ska ligga kvar så att man kan falla i den flera gånger.
         score = score - 10
-        g.set(37, 9, f" * You found a trap, You lost 10 points  **")
-        player.move(xi, yi)
+        g.set(37, 10, f" * You found a trap, You lost 10 points  **")
         g.set(player.pos_x, player.pos_y, g.empty)
         shovel = 0
 
@@ -98,10 +102,10 @@ def move_point(score, xi, yi, shovel, key, treasure, total_fruits_in_basket,exit
         pickups.randomtrap(g)   #- change trap place
 
     elif maybe_item == keyicon:     # found a key
-        g.set(37, 9, f" * You found a key :-)  **")
         player.move(xi, yi)
+        g.set(37, 10, f" * You found a key :-)  **")
         key +=1
-        g.set(50, 6, key)
+        g.set(50, 5, key)
         # erase key and save info
         g.set(player.pos_x, player.pos_y, g.empty)
 
@@ -109,96 +113,109 @@ def move_point(score, xi, yi, shovel, key, treasure, total_fruits_in_basket,exit
         player.move(xi, yi)
 
         if key >0 :
-            g.set(37, 9, f" * You found a treasure :-)  +100 points  **")
+            g.set(37, 10, f" * You found a treasure :-)  +100 points  **")
             g.clear(player.pos_x, player.pos_y)
-            # erase treasure  and save info
+            # erase treasure and save info
             treasure += 1
+            g.set(50, 6, treasure)
             score +=100
             key -=1
             if key == 0:
-                g.set(37, 9, f" * You have no more keys  :-(  **")
-            g.set(50, 6, key)
+                g.set(37, 10, f" * You have no more keys  :-(  **")
+            g.set(50, 5, key)
         else:
-            g.set(37, 9, f" * You have no keys for this treasure :-(  **")
+            g.set(37, 10, f" * You have no keys for this treasure :-(  **")
 
     else:
-        # print("OK", maybe_item)
-        player.move(xi, yi)
         # G) The floor is lava - för varje steg man går ska man tappa 1 poäng.
         score = score-1
-
+        jumpvalue=1
+        player.move(xi, yi)
         if isinstance(maybe_item, pickups.Item):
             # we found something
             score += maybe_item.value
       # Found fruit
-            g.set(37, 9, f"* You found a {maybe_item.name}, +{maybe_item.value} points.  ;-) ****")
+            g.set(37, 10, f"* You found a {maybe_item.name}, +{maybe_item.value} points.  ;-) ****")
             #g.set(player.pos_x, player.pos_y, g.empty)
             g.clear(player.pos_x, player.pos_y)
             inventory(fruit_list, maybe_item.name, player.pos_x, player.pos_y,key,treasure)
             # count total fruits found
             total_fruits_in_basket += 1
 
-    g.set(50, 7, score)   # show points
+
+    g.set(50, 8, score)   # show points
 
     # M) Exit - slumpa ett "E" på kartan. När man har plockat upp alla ursprungliga saker, kan man gå till exit
     if total_fruits == total_fruits_in_basket:
         exit_found ='exit_prel'
-        g.set(37, 9, f"* You can exit now   ;-) ****")
+        g.set(37, 10, f"* You can exit now   ;-) ****")
 
     if maybe_item == exit_door and exit_found == 'exit_prel':
         exit_found='canfinish'
 
 
-    return score,shovel, key, treasure, total_fruits_in_basket,exit_found
+    return score,shovel, key, treasure, total_fruits_in_basket,exit_found,jumpvalue
 
 
 
-def move_player (command, score, shovel, key, treasure,total_fruits_in_basket,exit_found):
-
+def move_player (command, score, shovel, key, treasure,total_fruits_in_basket,exit_found,jumpvalue):
     xi = 0
     yi = 0
 
-    # if command=="u":
+    # deprecated - changed to show the info in the grid
+    # if command=='u':
     #     show_score(score,key)
 
     if command=="i":
         showlist(fruit_list)
 
     if command=="a" or command=="d":
+
         if command=="a":
-            xi= -1
+            xi= -jumpvalue
         else:
-            xi= 1
-        temp=move_point(score,xi,yi,shovel,key,treasure,total_fruits_in_basket,exit_found)
+            xi= jumpvalue
+        temp=move_point(score,xi,yi,shovel,key,treasure,total_fruits_in_basket,exit_found,jumpvalue)
         score=temp[0]
         shovel=temp[1]
         key=temp[2]
         treasure=temp[3]
         total_fruits_in_basket = temp[4]
         exit_found=temp[5]
+        jumpvalue=temp[6]
 
 
     if command=="w" or command=="s":
+
         if command=="w":
-            yi= -1
+            yi= -jumpvalue
         else:
-            yi= 1
-        temp=move_point(score,xi,yi,shovel,key,treasure,total_fruits_in_basket,exit_found)
+            yi= jumpvalue
+        temp=move_point(score,xi,yi,shovel,key,treasure,total_fruits_in_basket,exit_found,jumpvalue)
         score = temp[0]
         shovel = temp[1]
         key = temp[2]
         treasure = temp[3]
         total_fruits_in_basket=temp[4]
         exit_found=temp[5]
-
+        jumpvalue=temp[6]
 
     if command=="k":
         shovel = 1
     # Set shovel
-        g.set(50, 3, ' [ On ]' )
+        g.set(50, 2, ' [ On ]' )
+
+    # N) Jump - om man skriver ett "J" innan något av "WASD", ska spelaren hoppa över en ruta.
+    #    Man förflyttar sig alltså två steg, men kan förstås bara plocka upp eller interagera med saker där man landar.
+    #    Hoppar man in i en vägg blir samman effekt som om man hade gått ett steg på vanligt sätt.
+    if command=="j":
+        jumpvalue=2
+        print('Ready to Jump :', jumpvalue)
+    else:
+        jumpvalue=1
 
 
-    return score,shovel, key, treasure, total_fruits_in_basket,exit_found
+    return score,shovel, key, treasure, total_fruits_in_basket,exit_found,jumpvalue
 
 
 # -------------------------------------------------------------STARTING-----------------
@@ -210,22 +227,22 @@ print("\n꧁∙·▫ₒₒ▫ᵒᴼᵒ▫ₒₒ▫꧁      Fruit basket for the 
 
 while not command.casefold() in ["q", "x"]:
     print_status(g)
-
-    # command = input("** Use WASD to move, Q/X to quit. ")
     command = input("                                   >> ")
     command = command.casefold()[:1]
 
-    resp=move_player(command, score,shovel,key,treasure, total_fruits_in_basket,exit_found )
+    resp=move_player(command, score,shovel,key,treasure, total_fruits_in_basket,exit_found,jumpvalue)
     score =resp[0]
     shovel =resp[1]
     key =resp[2]
     treasure =resp[3]
     total_fruits_in_basket = resp[4]
     exit_found = resp[5]
+    jumpvalue=resp[6]
+
     # print ('total_fruits: ',total_fruits)
     # print ('total_Basket:',total_fruits_in_basket)
     # print('exit_found', exit_found)
-    g.set(52, 4, f" = {total_fruits_in_basket}")
+    g.set(52, 3, f" = {total_fruits_in_basket}")
 
     steps +=1
     if steps == 25 and exit_found != 'exit_prel':
